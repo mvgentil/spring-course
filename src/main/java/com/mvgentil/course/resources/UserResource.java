c9248f932c8f305f74335034767b493bb8ad6192
Matheus Gentil <matheusvgentil@gmail.com>
2020-10-19 23:14:32 -0300

    User entity and resource

---
+++ src/main/java/com/mvgentil/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.mvgentil.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mvgentil.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Teste", "teste@gmail.com", "479999999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
